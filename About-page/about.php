5897b03728b72a8fddbf88de24424594a90b5e4d
Hafiz Atif <h.aofficial193@gmail.com>
2026-04-24 21:24:21 +0500

    About page completed

---
+++ About-page/about.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="about.css">
+</head>
+
+<body>
+    <!-- Section 1 - hero  -->
+
+    <section class="hero-section">
+        <div class="container firstblock">
+            <div class="title"data-aos="fade-right" data-aos-duration="2000">
+                <h1>About Our <span style="color: #FF4E2A;">Real Estate</span> Firm</h1>
+            </div>
+            <div class="details" data-aos="fade-left" data-aos-duration="2000">
+                <p>Secure your investment with transparent processes and verified property listings you can trust.
+                    Your safety and peace of mind come first, ensuring every transaction is smooth and protected.</p>
+                <div class="btns">
+                    <button class="btn joinbtn1">Join our team</button>
+                    <button class="btn joinbtn2">Know more</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- Section 2 - About_imagess -->
+
+    <section>
+        <div class="container imgs">
+            <img class="botom-img" src="About_images\Rectangle 1.png" data-aos="fade-right" data-aos-duration="2000" alt="top About_images">
+            <img class="top-img" src="About_images\Rectangle 2.png" data-aos="fade-left" data-aos-duration="2000" alt="bottom About_images">
+        </div>
+    </section>
+
+
+    <!-- Section 3 - review  -->
+
+    <section>
+        <div class="container overflow-hidden review ">
+            <h2 class="review-title" data-aos="fade-up" data-aos-duration="2000">Take a look at our numbers</h2>
+            <div class="cards">
+                <div class="card" data-aos="fade-right" data-aos-duration="2000">
+                    <h2 class="review-figers" >99%</h2>
+                    <h3>Customer satisfaction</h3>
+                    <p>Lorem ipsum dolor sit amet dolor sit consectetur adipiscing.</p>
+                </div>
+                <div class="card" data-aos="fade-up" data-aos-duration="2000">
+                    <h2 class="review-figers" >49k</h2>
+                    <h3>Successful Sales</h3>
+                    <p>Lorem ipsum dolor sit amet dolor sit consectetur adipiscing.</p>
+                </div>
+                <div class="card" data-aos="fade-left" data-aos-duration="2000">
+                    <h2 class="review-figers" >12M</h2>
+                    <h3>Real Estate Partners</h3>
+                    <p>Lorem ipsum dolor sit amet dolor sit consectetur adipiscing.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Section 4 - team  -->
+
+    <section class="team ">
+        <div class="container overflow-hidden">
+            <h2 class="team-title" data-aos="fade-up" data-aos-duration="2000">Meet our team members</h2>
+
+            <div class="team-cards">
+                <div class="member " data-aos="fade-right" data-aos-duration="2000">
+                    <img class="member-img " src="About_images/manager.png" alt="Manager">
+
+                    <div class="content">
+                        <h2 class="member-name">Tom Holland</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-up" data-aos-duration="2000">
+                    <img class="member-img" src="About_images/Financial Adviser.png" alt="Financial Adviser">
+
+                    <div class="content">
+                        <h2 class="member-name">Scarlett Johansson</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-left" data-aos-duration="2000">
+                    <img class="member-img" src="About_images/Tech lead.png" alt="Tech lead">
+
+                    <div class="content">
+                        <h2 class="member-name">Steve Rogers</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-right" data-aos-duration="2000">
+                    <img class="member-img" src="About_images/Manager 2.png" alt="Manager 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Chris Evans</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-up" data-aos-duration="2000">
+                    <img class="member-img" src="About_images/Financial Adviser 2.png" alt="Financial Adviser 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Sam Wilson</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-left" data-aos-duration="2000">
+                    <img class="member-img" src="About_images/Tech lead 2.png" alt="Tech lead 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Tony Stark</h2>
+                        <p class="member-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                        <img class="linkedin-icon" src="About_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section 4 - Articles  -->
+     <section>
+    <div class="container Articles">
+    <h1 data-aos="fade-left" data-aos-duration="2000">Articles & Resources</h1>
+
+    <div class="posts">
+      <div class="post" data-aos="fade-right" data-aos-duration="2000">
+        <img src="About_images/Rectangle 3.png" alt="">
+        <div class="post-content">
+          <h3>The Future of Real Estate: Trends to Watch Out for in 2023</h3>
+          <hr>
+          <div class="data">
+            <span>📅 April 23, 2026</span>
+            <span>⏱ 10 min read</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="post" data-aos="fade-up" data-aos-duration="2000">
+        <img src="About_images\Rectangle 4.png" alt="">
+        <div class="post-content">
+          <h3>Top 10 Cities for Real Estate Investment in 2023</h3>
+          <hr>
+          <div class="data">
+            <span>📅 April 23, 2026</span>
+            <span>⏱ 10 min read</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="post" data-aos="fade-left" data-aos-duration="2000">
+        <img src="About_images/Rectangle 5.png" alt="">
+        <div class="post-content">
+          <h3>Buying vs. Renting: Which Option Makes More Sense in Today’s Market?</h3>
+           <hr>
+          <div class="data">
+            <span>📅 April 23, 2026</span>
+            <span>⏱ 10 min read</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+
+    <!-- Section 4 - newz  -->
+     
+    <section>
+    <div class="newsletter">
+      <h2 data-aos="fade-right" data-aos-duration="2000">Subscribe to our weekly newsletter</h2>
+      <form class="newsletter-form" data-aos="fade-left" data-aos-duration="2000">
+        <input type="email"  placeholder="Enter email address">
+        <button class="btn" type="submit">Get in Touch</button>
+      </form>
+    </div>
+  </div>
+  </section>
+
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+   <script>
+  AOS.init();
+</script>
+</body>
+
+</html>
