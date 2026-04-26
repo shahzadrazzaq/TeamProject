eb509f0b4d09dd6aa668b7e15aa14a6577b89f29
Hafiz Atif <h.aofficial193@gmail.com>
2026-04-26 21:59:41 +0500

    Home page is pushed

---
+++ Home-page/home.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EstatePro - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+
+    <!-- section 1 - Home title & description -->
+
+    <section class=" home-section">
+        <div class="container home-block">
+            <div class="home-title" data-aos="fade-right" data-aos-duration="2000">
+                <h1>The best place to find your <span style="color: #ff4e2a;">dream house</span></h1>
+            </div>
+            <div class="home-desc">
+                <p class="home-desc-text" data-aos="fade-left" data-aos-duration="1500">We are real estate agency that will help you designing a modern and minimalist
+                    dream house, let’s
+                    discuss.</p>
+                <button type="button" data-aos="fade-left" data-aos-duration="2000">Get In Touch</button>
+                <div class="home-data" data-aos="fade-left" data-aos-duration="2500">
+                    <span>
+                        <h4>1200<span style="color: #ff4e2a;">+</span></h4>
+                        <p>Premium Product</p>
+                    </span>
+                    <span>
+                        <h4>4500<span style="color: #ff4e2a;">+</span></h4>
+                        <p>Happy Customer</p>
+                    </span>
+                    <span>
+                        <h4>240<span style="color: #ff4e2a;">+</span></h4>
+                        <p>Award Winning</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="home-sec-bottom" data-aos="fade-up" data-aos-duration="2000">
+            <form class="home-sec-form" data-aos="fade-left" data-aos-duration="2000">
+                <span><img src="Home-images/ci_location.png" alt="Location img  missing"></span>
+                <input type="text" placeholder="Search by location..." required>
+                <button class="btn">Search Now</button>
+            </form>
+        </div>
+    </section>
+
+
+
+    <!-- section 2 - Trust -->
+
+    <section class="trust">
+        <div class="container trust-block">
+            <div class="trust-title" data-aos="fade-right" data-aos-duration="2000">
+                <h2>Trusted by</h2>
+            </div>
+            <div class="trust-imgs" data-aos="fade-left" data-aos-duration="2000">
+                <img src="Home-images/image 42.png" alt="trust img missing">
+                <img src="Home-images/image 43.png" alt="trust img missing">
+                <img src="Home-images/image 44.png" alt="trust img missing">
+                <img src="Home-images/image 45.png" alt="trust img missing">
+                <img src="Home-images/image 41.png" alt="trust img missing">
+            </div>
+        </div>
+    </section>
+    <hr class="hr1">
+
+
+
+    <!-- section 4 - feedback -->
+
+    <section class="feedback">
+        <div class="feedback-title" data-aos="fade-up" data-aos-duration="2000">
+            <h2>What our clients say</h2>
+        </div>
+        <div class="feedback-content">
+            <img class="feedback-img" src="Home-images/image 40.png" alt="feedback-img missing" data-aos="fade-right" data-aos-duration="2000">
+            <div class="feedback-details" data-aos="fade-left" data-aos-duration="2000">
+                <h3>“They did an amazing job for our house.”</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque illo quo delectus recusandae
+                    debitis a aperiam, non, quis nostrum beatae vel culpa</p>
+                <div class="feedback-client">
+                    <img src="Home-images/Ellipse 198.png" alt="client img missing">
+                    <img class="feedback-line" src="Home-images/Rectangle 354.png" alt="missing">
+                    <h3>Tony Stark</h3>
+                </div>
+            </div>
+        </div>
+        <button class="btn" data-aos="fade-up" data-aos-duration="2000">Get In Touch <span><i class=" icon fa-solid fa-arrow-right"></i></span></button>
+    </section>
+
+
+
+    <!-- Section 5 - Dream home  -->
+
+    <section class="dream-home">
+        <div class="container dream-block">
+            <div class="dream-imgs">
+                <img class="dream-img1" data-aos="fade-right" data-aos-duration="2000" src="Home-images/Rectangle 8.png" alt="dream home img missing">
+                <img class="dream-img2" data-aos="fade-up" data-aos-duration="2000" src="Home-images/Rectangle 9.png" alt="dream home img missing">
+                <img class="dream-img3" data-aos="fade-left" data-aos-duration="2000" src="Home-images/Rectangle 10.png" alt="dream home img missing">
+            </div>
+            <div class="dream-details">
+                <h3 data-aos="fade-left" data-aos-duration="1500">With us help you find your dream home</h3>
+                <p data-aos="fade-left" data-aos-duration="2000">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis,
+                    lectus magna.</p>
+                <div class="dream-data" data-aos="fade-left" data-aos-duration="2500">
+                    <span class="dream-data1">
+                        <h2>127</h2>
+                        <p>Properties build</p>
+                    </span>
+                    <span class="dream-data2">
+                        <h2>300+</h2>
+                        <p>Happy Customers</p>
+                    </span>
+                    <span class="dream-data3">
+                        <h2>10</h2>
+                        <p>Years of experience</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- Section 6 - Articles & Resoucres  -->
+
+    <section class="articlesResoucres">
+
+        <h2 data-aos="fade-left" data-aos-duration="2000">Articles & Resources</h2>
+
+        <div class="article-content" >
+            <img class="article-img" src="Home-images/image 40.png" alt="article-img missing" data-aos="fade-right" data-aos-duration="2000">
+            <div class="article-details" data-aos="fade-left" data-aos-duration="2000">
+                <h3>Why a luxury home in a gated community is a better investment option</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit cilisis pretium leo pellentesque ultrices est
+                    varius amet rhoncus sed euismod sit id purus elementum nulla tincidunt massa vel.</p>
+                <hr>
+                <div class="data">
+                    <span>📅 April 23, 2026</span>
+                    <span>⏱ 10 min read</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="container articles">
+
+            <div class="posts">
+                <div class="post" data-aos="fade-right" data-aos-duration="2000">
+                    <img src="Home-images/Rectangle 3.png" alt="missing">
+                    <div class="post-content">
+                        <h3>The Future of Real Estate: Trends to Watch Out for in 2023</h3>
+                        <hr>
+                        <div class="data">
+                            <span>📅 April 23, 2026</span>
+                            <span>⏱ 10 min read</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post" data-aos="fade-up" data-aos-duration="2000">
+                    <img src="Home-images\Rectangle 4.png" alt="missing">
+                    <div class="post-content">
+                        <h3>Top 10 Cities for Real Estate Investment in 2023</h3>
+                        <hr>
+                        <div class="data">
+                            <span>📅 April 23, 2026</span>
+                            <span>⏱ 10 min read</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="post" data-aos="fade-left" data-aos-duration="2000">
+                    <img src="Home-images/Rectangle 5.png" alt="missing">
+                    <div class="post-content">
+                        <h3>Buying vs. Renting: Which Option Makes More Sense in Today’s Market?</h3>
+                        <hr>
+                        <div class="data">
+                            <span>📅 April 23, 2026</span>
+                            <span>⏱ 10 min read</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+
+
+    <!-- Section 7 - Newz  -->
+
+    <section>
+        <div class="newsletter">
+            <h2 data-aos="fade-right" data-aos-duration="2000">Subscribe to our weekly newsletter</h2>
+            <form class="newsletter-form" data-aos="fade-left" data-aos-duration="2000">
+                <input type="email" placeholder="Enter email address">
+                <button class="btn">Get in Touch</button>
+            </form>
+        </div>
+        </div>
+    </section>
+
+
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
