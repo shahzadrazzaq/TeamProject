21cb8bf79b2a4a38515cc6b5f7af5f61e517c176
Hafiz Atif <h.aofficial193@gmail.com>
2026-04-26 23:13:18 +0500

    Agent page is pushed

---
+++ Agents-page/agent.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="agent.css">
+</head>
+<body>
+
+
+    <!-- Section 4 - team  -->
+
+    <section class="team ">
+        <div class="container overflow-hidden">
+            <div class="team-title">
+            <h2 data-aos="fade-up" data-aos-duration="2000">Meet Our Agents</h2>
+            <p data-aos="fade-up" data-aos-duration="2500">Lorem ipsum dolor sit amet, consectetur adipiscing elit et dui tellus morbi integer neque, malesuada ac nulla tellu malesuada ac nulla tellu</p>
+            </div>
+
+            <div class="team-cards">
+                <div class="member " data-aos="fade-right" data-aos-duration="2000">
+                    <img class="member-img " src="Agent_images/atif.jpeg" alt="Manager">
+
+                    <div class="content">
+                        <h2 class="member-name">Atif Hameed</h2>
+                        <p class="member-desc">Reliable Operations Expert</p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-up" data-aos-duration="2000">
+                    <img class="member-img" src="Agent_images/umair.jpeg" alt="Financial Adviser">
+
+                    <div class="content">
+                        <h2 class="member-name">Muhammad Umair</h2>
+                        <p class="member-desc">Collaborative Tech Specialist </p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-left" data-aos-duration="2000">
+                    <img class="member-img" src="Agent_images/mohsin.jpeg" alt="Tech lead">
+
+                    <div class="content">
+                        <h2 class="member-name">Muhammad Mohsin</h2>
+                        <p class="member-desc">Strategic Growth Planner</p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-right" data-aos-duration="2000">
+                    <img class="member-img" src="Agent_images/Manager.png" alt="Manager 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Muhammad Subhan</h2>
+                        <p class="member-desc">Team Lead & Vision Driver</p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-up" data-aos-duration="2000">
+                    <img class="member-img" src="Agent_images/Financial Adviser.png" alt="Financial Adviser 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Muhammad AAkash</h2>
+                        <p class="member-desc">Client Success Champion</p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+                <div class="member" data-aos="fade-left" data-aos-duration="2000">
+                    <img class="member-img" src="Agent_images/Tech lead.png" alt="Tech lead 2">
+
+                    <div class="content">
+                        <h2 class="member-name">Muhammad Shahzad</h2>
+                        <p class="member-desc">Creative Problem Solver</p>
+
+                        <img class="linkedin-icon" src="Agent_images/linkedin icon.png" alt="LinkedIn profile">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Section 4 - newz  -->
+     
+    <section>
+    <div class="newsletter">
+      <h2 data-aos="fade-right" data-aos-duration="2000">Subscribe to our weekly newsletter</h2>
+      <form class="newsletter-form" data-aos="fade-left" data-aos-duration="2000">
+        <input type="email"  placeholder="Enter email address">
+        <button class="btn" type="submit">Get in Touch</button>
+      </form>
+    </div>
+  </div>
+  </section>
+
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+   <script>
+  AOS.init();
+</script>
+</body>
+
+</html>
